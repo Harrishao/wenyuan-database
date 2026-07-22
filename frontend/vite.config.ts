bcae8b5d7044b6ab00a067214dc5f0068b785008
Harrishao <cyutong558@gmail.com>
2026-07-22 22:25:29 +0800

    MVP 0存档

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 7777,
     proxy: {
       "/api": "http://localhost:4396",
--- start_all.bat
+++ start_all.bat
@@ -15,11 +15,11 @@ echo 数据库启动成功
 
 echo.
 echo [2/3] 启动后端服务...
-start "文渊后端服务" cmd /k "cd backend && if not exist .venv (python -m venv .venv && .venv\Scripts\python.exe -m pip install -e ".[dev]" && Copy-Item ..\.env.example .env && .venv\Scripts\alembic.exe upgrade head) && .venv\Scripts\uvicorn.exe app.main:app --reload --port 4396"
+start "文渊后端服务" cmd /k "cd backend && (if not exist .venv (python -m venv .venv && .venv\Scripts\python.exe -m pip install -e .[dev])) & (if not exist .env copy ..\.env.example .env) & .venv\Scripts\alembic.exe upgrade head & .venv\Scripts\uvicorn.exe app.main:app --reload --port 4396"
 
 echo.
 echo [3/3] 启动前端服务...
-start "文渊前端服务" cmd /k "cd frontend && if not exist node_modules (npm install) && npm run dev"
+start "文渊前端服务" cmd /k "cd frontend && (if not exist node_modules npm install) & npm run dev"
 
 echo.
 echo ==============================================
